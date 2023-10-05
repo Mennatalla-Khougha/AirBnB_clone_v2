f8f5af4ab1c8a4409ee7bf0482fd3109420987b4
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-05 23:29:09 +0300

    task 5

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,51 @@
+#sets up the web servers for the deployment of web_static.
+
+exec {'update':
+command  => 'apt-get update',
+provider => 'shell',
+}
+
+exec {'install':
+command  => 'apt-get install -y nginx',
+provider => 'shell',
+}
+
+exec {'mkdir':
+command  => 'mkdir -p /data/web_static/shared',
+provider => 'shell',
+}
+
+exec {'mkdir':
+command  => 'mkdir -p /data/web_static/releases/test',
+provider => 'shell',
+}
+
+exec {'echo':
+command  => 'echo "Hello world" >  /data/web_static/releases/test/index.html',
+provider => 'shell',
+}
+
+exec {'ln':
+command  => 'ln -sf /data/web_static/releases/test /data/web_static/current',
+provider => 'shell',
+}
+
+exec {'chown':
+command  => 'chown -R ubuntu:ubuntu /data/',
+provider => 'shell',
+}
+
+exec {'sed':
+command  => 'sed -i "/server_name _;/a \
+\
+    location /hbnb_static/ { \
+        alias /data/web_static/current/;\
+        index index.html;\
+    }" /etc/nginx/sites-available/default',
+provider => 'shell',
+}
+
+exec {'restart':
+command  => 'service nginx restart',
+provider => 'shell',
+}
