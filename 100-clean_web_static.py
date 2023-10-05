ee109aef02f370d6fc1d4189272b416a91fa8b26
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-05 22:59:07 +0300

    task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -64,10 +64,10 @@ def do_clean(number=0):
     """deletes out-of-date archives"""
     number = int(number)
     if number == 0:
-        number = 2
-    else:
-        number += 1
+        number = 1
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    number += 1
+
+    local('ls -dt versions/* | tail -n +{} | sudo xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs -d rm -rf'.format(path, number))
+    run('ls -dt {} | tail -n +{} | sudo xargs -d rm -rf'.format(path, number))
