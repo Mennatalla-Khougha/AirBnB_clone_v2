b153a109b1720a7b4fa76faeea1b8cf845e66229
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-20 18:38:35 +0300

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,12 +12,13 @@ def end_session(exception):
     """remove the current SQLAlchemy"""
     storage.close()
 
+
 @app.route("/states_list", strict_slashes=False)
 def List_of_states():
     """display HTML page"""
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
