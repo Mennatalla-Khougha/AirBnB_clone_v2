cf4484708f21a792c7098ac91bef50953645a101
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-10-22 21:31:09 +0000

    task 12

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-
+"""use storage for fetching data from the storage engine"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.user import User
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close(exception):
-    "close storage"
+def end_session(exception):
+    """remove the current SQLAlchemy"""
     storage.close()
 
 
-@app.route('/hbnb')
-def hbnb():
-    "displays a HTML page"
-    return render_template(
-        '100-hbnb.html',
-        states=storage.all(State).values(),
-        amenities_list=storage.all(Amenity).values(),
-        users=storage.all(User).values()
-    )
+@app.route("/hbnb", strict_slashes=False)
+def index():
+    """display am HTML page"""
+    states = storage.all(State).values()
+    amenities=storage.all(Amenity).values()
+    users = storage.all(User).values()
+    return render_template("100-hbnb.html", states=states, amenities=amenities, users=users)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
